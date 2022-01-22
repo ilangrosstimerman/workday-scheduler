0ac00f316b0fe492826c4e86402e19cee43b12ed
ilantimerman <ilan.tmrmn@gmail.com>
2022-01-21 21:37:20 -0700

    edit events and save/load them from local

--- assets/js/application.js
+++ assets/js/application.js
@@ -10,3 +10,34 @@ function createEvent(rowId) {
     textAreaEl.val("your event");
     $("#" + rowId).append(textAreaEl);
 }
+
+//edit existing events
+
+function editEvent(rowId) {
+    let eventEl = $("#" + rowId).children();
+    let eventText = eventEl.text();
+    let newEventEl = $("<textarea>").text(eventText);
+    eventEl.replaceWith(newEventEl);
+    newEventEl.focus();
+}
+
+//save information to local
+
+function saveEvent() {
+    localStorage.setItem("events", JSON.stringify(savedEvents));
+}
+
+//load information from local
+
+function loadEvents() {
+    savedEvents = JSON.parse(localStorage.getItem("events"));
+    if (!savedEvents) {
+        savedEvents = [];
+    }
+}
+$.each(savedEvents, function(index) {
+    let eventText = savedEvents[index].eventVal;
+    let rowId = savedEvents[index].rowId;
+    let eventEl = $("<p>").text(eventText);
+    $(`.event#${rowId}`).append(eventEl);
+ });
